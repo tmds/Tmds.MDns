54cff669cfd3e89c59e0dde9b5d72ea2c999252c
Tom Deseyn <tom.deseyn@gmail.com>
2017-12-01 13:09:44 +0100

    Migrate to csproj (#16)

--- build.cmd
+++ build.cmd
@@ -1,2 +1,2 @@
-@ECHO OFF
-PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& '%~dp0build.ps1' %*; exit $LASTEXITCODE"
+dotnet restore src/Tmds.MDns
+dotnet pack -c Release src/Tmds.MDns
