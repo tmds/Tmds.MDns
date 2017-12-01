c2b275864f514b32cf31d9f3da4a9f3261cd1921
Tom Deseyn <tom.deseyn@gmail.com>
2017-12-01 11:10:05 +0100

    Remove specific build scripts

--- build.cmd
+++ build.cmd
@@ -1,2 +1,1 @@
-@ECHO OFF
-PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& '%~dp0build.ps1' %*; exit $LASTEXITCODE"
+dotnet pack -c Release src/Tmds.MDns
